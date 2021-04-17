013773c4c298fafdabf1a18225849f56c3ca27af
mmmz21 <emmawilcox121@gmail.com>
2021-04-17 00:55:42 -0700

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -21,28 +21,29 @@ function App() {
 
   const Statistic = ({ text, value }) => {
     return (
-      <div>
-        <p> {text} {value} </p>
-      </div>
+      <tr>
+        <td> {text} </td>
+        <td> {value} </td>
+      </tr>
     )
   }
   const Statistics = ({ good, neutral, bad }) => {
-    if (good + neutral + bad == 0) {
+    if (good + neutral + bad === 0) {
       return (
-        <div>
-          <p> No feedback given </p>
-        </div>
+        <><p> No feedback given </p></>
       )
     }
     return (
-      <div>
-        <Statistic text={"good"} value={good} />
-        <Statistic text={"neutral"} value={neutral} />
-        <Statistic text={"bad"} value={bad} />
-        <Statistic text={"all"} value={good + neutral + bad} />
-        <Statistic text={"average"} value={(good - bad) / (good + neutral + bad)} />
-        <Statistic text={"positive"} value={good / (good + neutral + bad) + "%"} />
-      </div>
+      <table>
+        <tbody>
+          <Statistic text={"good"} value={good} />
+          <Statistic text={"neutral"} value={neutral} />
+          <Statistic text={"bad"} value={bad} />
+          <Statistic text={"all"} value={good + neutral + bad} />
+          <Statistic text={"average"} value={(good - bad) / (good + neutral + bad)} />
+          <Statistic text={"positive"} value={good / (good + neutral + bad) + "%"} />
+        </tbody>
+      </table>
     )
   }
 
